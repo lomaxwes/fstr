7eaa8c095e32f58aede27788488873843040768e
lomaxwes <lomaxwes@gmail.com>
2023-06-04 10:54:09 +0500

    функция добавления пользователя

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+import uvicorn
 
+from fastapi import FastAPI
+from models import Base, User, Coords, Level, Pereval, PerevalImages
 
 app = FastAPI()
+
 
 @app.get("/")
 def hello():
@@ -10,3 +13,10 @@ def hello():
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+def create_user(db, user_data):
+    user = User(email=user_data['email'], fam=user_data['fam'], otc=user_data['otc'],
+                name=user_data['name'], phone=user_data['phone'])
+    db.add(user)
+    return user
